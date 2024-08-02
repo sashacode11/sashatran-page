a5c6e5aeff92d7e352387685fcae7293747e91d3
Kevin Rethwisch <kevinrethwisch@gmail.com>
2024-08-02 16:50:36 +0700

    final commit

--- public/js/script.js
+++ public/js/script.js
@@ -43,29 +43,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
-
-// Contact
-// document.addEventListener('DOMContentLoaded', function () {
-//   document
-//     .getElementById('contactForm')
-//     .addEventListener('submit', function (event) {
-//       event.preventDefault();
-
-//       const formData = {
-//         name: document.getElementById('name').value,
-//         email: document.getElementById('email').value,
-//         message: document.getElementById('message').value,
-//       };
-
-//       fetch('/submit-form', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(formData),
-//       })
-//         .then(response => response.text())
-//         .then(data => alert(data))
-//         .catch(error => console.error('Error:', error));
-//     });
-// });
